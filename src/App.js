1686f5fb793490730f456d8150053460138106b0
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-02-04 17:36:34 +0200

    :sparkles: Retrieve credentials validation from GUC API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
 import logo from './guc-logo.png';
 import './App.css';
 
@@ -12,6 +13,20 @@ class App extends Component {
     };
   }
 
+  login = () => {
+    const loginUrl = 'http://guc-api.herokuapp.com/api/login';
+    const options = {
+      headers: {
+        'Authorization': `Basic ${btoa(this.state.username.concat(':').concat(this.state.password))}`
+      }
+    };
+
+    fetch(loginUrl, options)
+      .then(res => res.json())
+      .then(json => alert(json.data.authorized))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div className="App">
@@ -21,12 +36,12 @@ class App extends Component {
         </div>
         <div>
           <h3>Username:</h3>
-          <input type="text" onChange={(event) => { this.setState({ username: event.target.value }) } } />
+          <input type="text" onChange={event => this.setState({ username: event.target.value })} />
           <h3>Password:</h3>
-          <input type="password" onChange={(event) => { this.setState({ password: event.target.value }) } } />
+          <input type="password" onChange={event => this.setState({ password: event.target.value })} />
           <br />
           <br />
-          <button onClick={() => {/*TODO: Perform HTTP request*/ } }>Log in</button>
+          <button onClick={() => this.login()}>Log in</button>
         </div>
       </div>
     );
